c6131d29eff50384484534b49dbbbfa7baef9e5d
Guy Naamati <gnaamati@codon-login-01.ebi.ac.uk>
2022-05-09 12:03:55 +0100

    small changes

--- run_red_repeats.sh
+++ run_red_repeats.sh
@@ -2,8 +2,9 @@ cd /nfs/production/flicek/ensembl/plants/repeatdetector
 
 pyenv shell 3.7.9 plant-scripts
 
-species=thinopyrum_elongatum
-db=thinopyrum_elongatum_core_53_106_1
+#species=thinopyrum_elongatum
+species=lactuca_sativa
+db=lactuca_sativa_core_53_106_1
 #genome_path=/nfs/production/panda/ensemblgenomes/data/Plants/repeatdetector/genomes
 genome_path=/nfs/production/flicek/ensembl/plants/repeatdetector    
 genome=$species.dna.fasta
